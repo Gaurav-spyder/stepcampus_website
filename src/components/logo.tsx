29ca3e7aa11cd49acc2dc944a15b5240a876ae44
Gaurav Gupta <g.guptabih@gmail.com>
2025-08-05 16:48:55 +0000

    i ahve uploaded logo.png on public folder, remove logo.svg and go all ch

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-       <Image src="/logo.svg" width={40} height={40} alt="StepCampus" className="rounded-full" />
+       <Image src="/logo.png" width={40} height={40} alt="StepCampus" className="rounded-full" />
       <span className="text-xl font-headline font-bold text-foreground">
         StepCampus
       </span>
